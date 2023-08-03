a64cfa1597c81ada9d2a6ad9b2fc8bfd68398fa5
Cookie <pon05114@naver.com>
2023-08-03 16:06:25 +0900

    feat: deploy 6weeks files
    - 6주차과제 제출

--- src/test/java/com/example/kakao/order/OrderRestControllerTest.java
+++ src/test/java/com/example/kakao/order/OrderRestControllerTest.java
@@ -1,92 +1,110 @@
-package com.example.kakao.product;
+package com.example.kakao.order;
 
 import com.example.kakao.MyRestDoc;
+import com.example.kakao.user.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @ActiveProfiles("test")
-@Sql("classpath:db/teardown.sql")
+@Sql(value = "classpath:db/teardown.sql")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class ProductRestControllerTest extends MyRestDoc {
+public class OrderRestControllerTest extends MyRestDoc {
 
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private MockMvc mvc;
+
+    // (기능9) 결재하기 - (주문 인서트) POST
     @Test
-    public void findAll_test() throws Exception {
-        // given teardown.sql
+    @WithUserDetails(value = "ssarmango@nate.com")
+    public void save_test() throws Exception{
+        // given -> WithuserDetails
 
         // when
         ResultActions resultActions = mvc.perform(
-                get("/products")
+            post("/carts/orders/save")
+                    .contentType(MediaType.APPLICATION_JSON)
         );
+
 
         // console
         String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         System.out.println("테스트 : "+responseBody);
 
-        // verify
+        // then
         resultActions.andExpect(jsonPath("$.success").value("true"));
-        resultActions.andExpect(jsonPath("$.response[0].id").value(1));
-        resultActions.andExpect(jsonPath("$.response[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
-        resultActions.andExpect(jsonPath("$.response[0].description").value(""));
-        resultActions.andExpect(jsonPath("$.response[0].image").value("/images/1.jpg"));
-        resultActions.andExpect(jsonPath("$.response[0].price").value(1000));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
+
     }
 
+    // (기능10) 주문 결과 확인 GET
     @Test
-    public void findById_test() throws Exception {
-        // given teardown.sql
-        int id = 1;
+    @WithUserDetails(value = "ssarmango@nate.com")
+    public void findById_test() throws Exception{
+        // given
+        int orderId = 1;
 
         // when
         ResultActions resultActions = mvc.perform(
-                get("/products/" + id)
+                get("/carts/orders/{id}", orderId)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
         );
+
 
         // console
         String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         System.out.println("테스트 : "+responseBody);
 
-        // verify
+        // then
         resultActions.andExpect(jsonPath("$.success").value("true"));
         resultActions.andExpect(jsonPath("$.response.id").value(1));
-        resultActions.andExpect(jsonPath("$.response.productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
-        resultActions.andExpect(jsonPath("$.response.description").value(""));
-        resultActions.andExpect(jsonPath("$.response.image").value("/images/1.jpg"));
-        resultActions.andExpect(jsonPath("$.response.price").value(1000));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
     }
 
+    // (기능10) 주문 결과 확인 GET
     @Test
-    public void findById_error_test() throws Exception {
-        // given teardown.sql
-        int id = 0;
+    @WithUserDetails(value = "ssarmango@nate.com")
+    public void findById_error_test() throws Exception{
+        // given
+        int orderId = 2;
 
         // when
         ResultActions resultActions = mvc.perform(
-                get("/products/" + id)
+                get("/carts/orders/{id}", orderId)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
         );
+
 
         // console
         String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         System.out.println("테스트 : "+responseBody);
 
-        // verify
+        // then
         resultActions.andExpect(jsonPath("$.success").value("false"));
-        resultActions.andExpect(jsonPath("$.error.message").value("해당 상품을 찾을 수 없습니다 : " + id));
+        resultActions.andExpect(jsonPath("$.error.message").value("해당된 아이템을 찾을 수 없습니다."));
         resultActions.andExpect(jsonPath("$.error.status").value("404"));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
 
-
-        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);// API DOCS부분
     }
 }
